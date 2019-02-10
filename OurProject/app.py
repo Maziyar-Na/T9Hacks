b60350ec7ac58d7bfb6d329dddc0ab3cf6b93d9a
Maziyar <maziyar.nazary@gmail.com>
2019-02-09 20:40:13 -0700

    Organized

--- OurProject/app.py
+++ OurProject/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home(name=None):
-    return render_template("farmer.html", name=name)
+    return render_template("search.html", name=name)
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000, debug=True)
